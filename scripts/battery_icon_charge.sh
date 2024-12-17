3ea214f9f35c717fd72a962c5ff1e8ca60da43c9
slange-dev <74963785+slange-dev@users.noreply.github.com>
2024-12-17 17:22:10 +0100

    Update battery_icon_charge.sh
    Add more charge_tiers
    Add NerdFont icons (Testing)

--- scripts/battery_icon_charge.sh
+++ scripts/battery_icon_charge.sh
@@ -5,6 +5,8 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 # script global variables
+icon_charge_tier10=''
+icon_charge_tier9=''
 icon_charge_tier8=''
 icon_charge_tier7=''
 icon_charge_tier6=''
@@ -13,19 +15,25 @@ icon_charge_tier4=''
 icon_charge_tier3=''
 icon_charge_tier2=''
 icon_charge_tier1=''
+icon_charge_tier0=''
 
 # script default variables
-icon_charge_tier8_default='█'
-icon_charge_tier7_default='▇'
-icon_charge_tier6_default='▆'
-icon_charge_tier5_default='▅'
-icon_charge_tier4_default='▄'
-icon_charge_tier3_default='▃'
-icon_charge_tier2_default='▂'
-icon_charge_tier1_default='▁'
+icon_charge_tier10_default='󱟢'
+icon_charge_tier9_default='󰂂'
+icon_charge_tier8_default='󰂁'
+icon_charge_tier7_default='󰂀'
+icon_charge_tier6_default='󰁿'
+icon_charge_tier5_default='󰁾'
+icon_charge_tier4_default='󰁽'
+icon_charge_tier3_default='󰁼'
+icon_charge_tier2_default='󰁻'
+icon_charge_tier1_default='󰁺'
+icon_charge_tier0_default='󰂎'
 
 # icons are set as script global variables
 get_icon_charge_settings() {
+	icon_charge_tier10=$(get_tmux_option "@batt_icon_charge_tier10" "$icon_charge_tier10_default")
+	icon_charge_tier9=$(get_tmux_option "@batt_icon_charge_tier9" "$icon_charge_tier9_default")
 	icon_charge_tier8=$(get_tmux_option "@batt_icon_charge_tier8" "$icon_charge_tier8_default")
 	icon_charge_tier7=$(get_tmux_option "@batt_icon_charge_tier7" "$icon_charge_tier7_default")
 	icon_charge_tier6=$(get_tmux_option "@batt_icon_charge_tier6" "$icon_charge_tier6_default")
@@ -34,27 +42,36 @@ get_icon_charge_settings() {
 	icon_charge_tier3=$(get_tmux_option "@batt_icon_charge_tier3" "$icon_charge_tier3_default")
 	icon_charge_tier2=$(get_tmux_option "@batt_icon_charge_tier2" "$icon_charge_tier2_default")
 	icon_charge_tier1=$(get_tmux_option "@batt_icon_charge_tier1" "$icon_charge_tier1_default")
+	icon_charge_tier0=$(get_tmux_option "@batt_icon_charge_tier0" "$icon_charge_tier0_default") 
 }
 
 print_icon_charge() {
 	percentage=$($CURRENT_DIR/battery_percentage.sh | sed -e 's/%//')
-	if [ $percentage -ge 95 -o "$percentage" == "" ]; then
-		# if percentage is empty, assume it's a desktop
-		printf "$icon_charge_tier8"
+ 	# if percentage is empty, assume it's a desktop
+ 	if [ -o "$percentage" == "" ]; then
+  		printf ""
+	elif [ $percentage -gt 95 ]; then
+		printf "$icon_charge_tier10"  
+  	elif [ $percentage -ge 90 ]; then
+		printf "$icon_charge_tier9"
 	elif [ $percentage -ge 80 ]; then
-		printf "$icon_charge_tier7"
-	elif [ $percentage -ge 65 ]; then
+		printf "$icon_charge_tier8"  
+	elif [ $percentage -ge 70 ]; then
+		printf "$icon_charge_tier7"  
+	elif [ $percentage -ge 60 ]; then
 		printf "$icon_charge_tier6"
 	elif [ $percentage -ge 50 ]; then
 		printf "$icon_charge_tier5"
-	elif [ $percentage -ge 35 ]; then
+	elif [ $percentage -ge 40 ]; then
 		printf "$icon_charge_tier4"
-	elif [ $percentage -ge 20 ]; then
+	elif [ $percentage -ge 30 ]; then
 		printf "$icon_charge_tier3"
-	elif [ $percentage -gt 5 ]; then
+	elif [ $percentage -ge 20 ]; then
 		printf "$icon_charge_tier2"
-	else
+	elif [ $percentage -ge 10 ]; then
 		printf "$icon_charge_tier1"
+	else
+		printf "$icon_charge_tier0"
 	fi
 }
 
